c42f991389b02c2c3360118c74d5c1212e1d1b10
Neamoth <Neamoth15-8202@diu.edu.bd>
2024-12-19 10:52:25 +0600

    dom: create element, appendChild, Childs

--- dom/dom.js
+++ dom/dom.js
@@ -1,1 +1,19 @@
 console.log("-----DOM Practices-----");
+
+// So we can create a new dom without help of body but it will works properties with js
+// declearations only
+const dom = document.createElement("div");
+dom.id = "dom";
+
+dom.textContent = "Hello heading";
+dom.style.backgroundColor = "rgba(255,255,255)"
+
+document.body.appendChild(dom);
+
+
+// child
+
+const [a,b,c] = ["Neamoth", "Nahid", "Saima"]
+
+document.getElementById('li').firstChild.nodeValue = a
+document.getElementById('li').nextSibling.nodeValue =  b
